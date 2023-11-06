65bdccc441793b2b20c63ded42964f4197c30fb0
Forrest Storrs <jstorrs@niche.com>
2023-11-06 16:22:15 -0500

    navbar styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,20 +5,25 @@ import navLinks from "../constants/navLinks";
 import { TNavLinks } from '../types/types'
 
 const Navbar = () => {
+  const renderNavLinks = () => (
+    Object.keys(navLinks).map((key) => {
+      const navLink = navLinks[key as keyof TNavLinks]
+      
+      return (
+        <Link className='navbar--link' key={navLink.label} href={navLink.target}>
+          {navLink.label}
+        </Link>
+      )
+    })
+  )
+  
   return (
     <div className="navbar">
       <div className="navbar--title">
         <Link href="/">Forrest Storrs</Link>
       </div>
-
       <div className="navbar--links">
-        {Object.keys(navLinks).map((navLink) => (
-          <li key={navLinks[navLink as keyof TNavLinks].label}>
-            <Link href={navLinks[navLink as keyof TNavLinks].target}>
-              {navLinks[navLink as keyof TNavLinks].label}
-            </Link>
-          </li>
-        ))}
+        {renderNavLinks()}
       </div>
     </div>
   );
