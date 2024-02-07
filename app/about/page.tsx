04af95907fd8f1fdfaabf7cb01df56b387316fb0
Forrest Storrs <jstorrs@niche.com>
2024-02-07 13:51:03 -0500

    started refactoring content header styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import ContentHeader from '@/common/components/ContentHeader'
+import ContentHeaderV2 from '@/common/components/ContentHeaderV2'
 import { aboutHeader } from '@/common/constants/headerBlocks'
 
 const About = () => {
     return (
         <main>
-            <ContentHeader block={aboutHeader} />
+            {/* <ContentHeader block={aboutHeader} /> */}
+            <ContentHeaderV2 block={aboutHeader} />
         </main>
     )
 }
---
+++ common/components/ContentHeaderV2.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Courier_Prime, Oswald } from "next/font/google";
+import { TDescriptionBlock } from "../types/types";
+
+import Headshot from '../assets/Headshot.png'
+
+const oswald = Oswald({
+  subsets: ["latin"],
+});
+
+const courierPrime = Courier_Prime({
+  subsets: ['latin'],
+  weight: ["400", "700"],
+})
+
+interface TProps {
+  block: TDescriptionBlock
+  isReaderLoaded?: boolean
+  openReaderCallback?: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ContentHeaderV2 = (props: TProps) => {
+  const { block, isReaderLoaded, openReaderCallback } = props;
+
+  return (
+    <div className={`${oswald.className} content-header-v2--wrapper`}>
+      <div className="content-header-v2--cover-wrapper">
+        <Image className='content-header-v2--cover-image' src={block.coverImage} alt={block.coverAltText} fill={true} />
+      </div>
+      <div className="content-header-v2--description">
+        {block.title && <h1>{block.title}</h1>}
+        {block.subtitle && <h2>{block.subtitle}</h2>}
+        {block.bodyText && <p>{block.bodyText}</p>}
+        {block.links && (
+          <div className={`${courierPrime.className} content-header-v2--description__links-wrapper`}>
+            {block.links.map((link, i) => {
+              return (
+                <div className='content-header-v2--description__link-wrapper' key={link.label}>
+                  { openReaderCallback && !isReaderLoaded && (
+                    <div onClick={() => openReaderCallback(true)} className="content-header-v2--description__link">Read</div>
+                  )}
+                  <Link
+                    className="content-header-v2--description__link"
+                    href={link.target}
+                  >
+                    {link.label}
+                  </Link>
+                  {/* @ts-expect-error - can't get here if undefined */}
+                  {i < block.links.length - 1 && (
+                    <div className="content-header-v2--description__links-spacer">|&nbsp;</div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ContentHeaderV2;
