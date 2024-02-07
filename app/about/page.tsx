49d726418c681ee7ce702a8014029c1bfe455821
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2024-02-07 16:42:16 -0500

    used css grid to refactor header content styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 import ContentHeader from '@/common/components/ContentHeader'
-import ContentHeaderV2 from '@/common/components/ContentHeaderV2'
 import { aboutHeader } from '@/common/constants/headerBlocks'
 
 const About = () => {
     return (
         <main>
-            {/* <ContentHeader block={aboutHeader} /> */}
-            <ContentHeaderV2 block={aboutHeader} />
+            <ContentHeader block={aboutHeader} />
         </main>
     )
 }
