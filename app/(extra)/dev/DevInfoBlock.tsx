53b621957546b9f68c278fe17c9eae92a9c22bca
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2025-02-02 13:37:42 -0500

    adding map keys

--- app/(extra)/dev/DevInfoBlock.tsx
+++ app/(extra)/dev/DevInfoBlock.tsx
@@ -12,16 +12,16 @@ const DevInfoBlock = (props: TProps) => {
     return (
         <div className="dev-info-block">
             {block.header && <div className="dev-info-block__header">{block.header}</div>}
-            {block.sections && block.sections.map(section => (
-                <div className="dev-block-section">
+            {block.sections && block.sections.map((section, i) => (
+                <div key={`section${i + 1}`} className="dev-block-section">
                     {section.preHeader && <div className="dev-block-section__pre-header">{section.preHeader}</div>}
                     {section.sectionHeader && <div className="dev-block-section__header">{section.sectionHeader}</div>}
                     {section.subHeader && <div className="dev-block-section__sub-header">{section.subHeader}</div>}
                     {section.bodyText && <div className="dev-block-section__body-text">{section.bodyText}</div>}
                     {section.bullets &&
                         <ul className="dev-block-section__bullet-list">
-                            {section.bullets.map(bullet => (
-                                <li className="dev-block-section__bullet">{bullet}</li>
+                            {section.bullets.map((bullet, i) => (
+                                <li key={`bullet${i + 1}`} className="dev-block-section__bullet">{bullet}</li>
                             ))}
                         </ul>
                     }
