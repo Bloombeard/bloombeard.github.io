de017a882a78912357572ddc3f0ff36d70b11036
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2025-03-23 22:17:36 -0400

    changed to retina pictures, added borders on desktop

--- app/(extra)/cards/page.tsx
+++ app/(extra)/cards/page.tsx
@@ -1,22 +1,31 @@
 'use client'
 
 import React, { useState } from 'react'
+import Image from 'next/image'
 import CardsAppCardPicker from '@/common/components/CardsApp/CardsAppCardPicker'
 import CardsAppHeader from '@/common/components/CardsApp/CardsAppHeader'
 import CardsAppAbout from '@/common/components/CardsApp/CardsAppAbout'
+import border1 from '@/public/assets/cards/border-1.jpeg'
+import border2 from '@/public/assets/cards/border-2.jpeg'
+import border3 from '@/public/assets/cards/border-3.jpeg'
+import border4 from '@/public/assets/cards/border-4.jpeg'
 
 const Cards = () => {
     const [isPlayView, setIsPlayView] = useState<boolean>(true)
 
     return (
         <main>
-            <CardsAppHeader isPlayView={isPlayView} setIsPlayViewCallback={setIsPlayView} />
-            {isPlayView ?
-                <CardsAppCardPicker />
-                :
-                <CardsAppAbout />
-            }
-            <span className="cards-app--copyright-footer">Created in 2025 by <a href="https://www.forreststorrs.com">Forrest Storrs</a> and <a href="https://www.illustratornate.com/">Nate Taylor</a></span>
+            <Image className="cards-border" src={isPlayView ? border3 : border1} alt="left border image" />
+            <div className="cards-content">
+                <CardsAppHeader isPlayView={isPlayView} setIsPlayViewCallback={setIsPlayView} />
+                {isPlayView ?
+                    <CardsAppCardPicker />
+                    :
+                    <CardsAppAbout />
+                }
+                <span className="cards-app--copyright-footer">Created in 2025 by <a href="https://www.forreststorrs.com">Forrest Storrs</a> and <a href="https://www.illustratornate.com/">Nate Taylor</a></span>
+            </div>
+            <Image className="cards-border" src={isPlayView ? border4 : border2} alt="right border image" />
         </main>
     )
 }
