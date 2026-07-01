b28166302d16c2b6d26d769599144ea7215da56e
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2026-07-01 15:18:42 -0400

    ring images added. Fixed in place

--- app/(extra)/wedding/page.tsx
+++ app/(extra)/wedding/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import React, { useEffect, useRef, useState } from 'react'
-
+import Image from 'next/image'
 import { Tangerine } from 'next/font/google'
+
+import weddingRingTop from '@/public/assets/wedding/weddingRingTop.png'
+import weddingRingBottom from '@/public/assets/wedding/weddingRingBottom.png'
 
 const tangerine = Tangerine({
     adjustFontFallback: false,
@@ -44,28 +47,32 @@ const Wedding = () => {
 
     return (
         <main>
-            { hasAccess ? (
-                <div className="center-column">
-                    <div className="info-1">
-                        <div className={tangerine.className} style={{ fontSize: '64px' }}>You are invited</div>
-                        <div style={{ fontSize: `${defaultFontSize}`}}>to a much belated wedding between</div>
+            {/* { hasAccess ? ( */}
+                <>
+                    <div className="center-column">
+                        <div className="info-1">
+                            <div className={tangerine.className} style={{ fontSize: '64px' }}>You are invited</div>
+                            <div style={{ fontSize: `${defaultFontSize}`}}>to a much belated wedding between</div>
+                        </div>
+                        <div style={{ fontSize: `${largestFontSize}`, fontWeight: 'bold' }}>Zhen & Forrest</div>
+                        <div className="info-2">
+                            <div style={{ fontSize: `${defaultFontSize}`}}>on October the 17th, after nightfall</div>
+                            <div style={{ fontSize: `${defaultFontSize}`}}>with a reception to follow on the 18th.</div>
+                        </div>
+                        <div style={{ fontSize: `${defaultFontSize}`}}>More details to follow.</div>
+                        <div style={{ fontSize: `${largerFontSize}` }}>Save the date!</div>
                     </div>
-                    <div style={{ fontSize: `${largestFontSize}`, fontWeight: 'bold' }}>Zhen & Forrest</div>
-                    <div className="info-2">
-                        <div style={{ fontSize: `${defaultFontSize}`}}>on October the 17th, after nightfall</div>
-                        <div style={{ fontSize: `${defaultFontSize}`}}>with a reception to follow on the 18th.</div>
-                    </div>
-                    <div style={{ fontSize: `${defaultFontSize}`}}>More details to follow.</div>
-                    <div style={{ fontSize: `${largerFontSize}` }}>Save the date!</div>
-                </div>   
-            ) : (
+                        <Image objectFit="cover" className="ring ring-top" src={weddingRingTop} alt="ring of animals, top" />
+                        <Image objectFit="cover" className="ring ring-bottom" src={weddingRingBottom} alt="ring of animals, bottom" />
+                </>
+            {/* ) : (
                 <div className="login-wrapper">
                     <div>please enter password</div>
                     <input onChange={ (e) => setPasswordInput(e.target.value)} ref={inputRef} value={passwordInput} />
                     <button onClick={onLoginClick}>enter</button>
                     { passwordMissed && (<div style={{ color: 'red' }}>password incorrect</div>)}
                 </div>
-            )}
+            )} */}
         </main>
     )
 }
